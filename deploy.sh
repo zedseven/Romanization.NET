ed6665f96dee75669d5a373c80ff60eae9c1aadc
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2020-10-19 10:25:19 -0400

    Updated travis.yml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ PackagesSource=$4
 cd Romanization
 
 echo "Downloading the latest nuget.exe..."
-curl https://dist.nuget.org/win-x86-commandline/latest/nuget.exe --output ./nuget.exe
+curl https://dist.nuget.org/win-x86-commandline/latest/nuget.exe --output ./nuget.exe -s
+chmod +x ./nuget.exe
 
 echo "Packing built library..."
 ./nuget.exe pack ./Romanization.csproj -Prop Configuration=Release -Verbosity detailed
