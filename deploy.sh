d0c9d25431b619338c25072f381054818d93782c
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2020-10-19 10:51:05 -0400

    Updated deploy.sh *again*.

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,15 @@ cd Romanization
 #chmod +x ./nuget.exe
 
 echo "Packing built library..."
-dotnet pack ./Romanization.csproj --configuration Release --verbosity detailed
+dotnet pack ./Romanization.nuspec --configuration Release
+
+pattern="Romanization.NET.*.nupkg"
+files=( $pattern )
+echo "${files[0]}"
 
 echo "Pushing packed package to Nuget..."
-dotnet nuget push ./Romanization.NET.*.nupkg --api-key "$NugetApiKey" --source "$NugetSource"
+dotnet nuget push "${files[0]}" --api-key "$NugetApiKey" --source "$NugetSource"
 echo "Pushing packed package to GitHub Packages..."
-dotnet nuget push ./Romanization.NET.*.nupkg --api-key "$PackagesApiKey" --source "$PackagesSource"
+dotnet nuget push "${files[0]}" --api-key "$PackagesApiKey" --source "$PackagesSource"
 
 echo "Done!"
