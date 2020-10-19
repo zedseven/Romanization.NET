ee5dbf9934e70ef45d74af63935d2fdff36608fe
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2020-10-19 11:51:22 -0400

    Updated deploy.sh *again*.

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,8 @@ files=( Romanization.NET.*.nupkg )
 echo "${files[0]}"
 
 echo "Pushing packed package to Nuget..."
-dotnet nuget push "${files[0]}" --api-key "$NugetApiKey" --source "$NugetSource"
+dotnet nuget push "${files[0]}" --api-key "$NugetApiKey" --source "$NugetSource" --no-service-endpoint
 echo "Pushing packed package to GitHub Packages..."
-dotnet nuget push "${files[0]}" --api-key "$PackagesApiKey" --source "$PackagesSource"
+dotnet nuget push "${files[0]}" --api-key "$PackagesApiKey" --source "$PackagesSource" --no-service-endpoint
 
 echo "Done!"
