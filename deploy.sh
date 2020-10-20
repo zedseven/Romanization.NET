74fb9ce111db1506ed7c58b7edfddda0072c4c9f
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2020-10-19 20:58:13 -0400

    Updated deploy.sh *again*, and incremented minor version to see if XML documentation is finally included in the nupkg.

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,10 @@ files=( Romanization.NET.*.nupkg )
 echo "${files[0]}"
 
 echo "Pushing packed package to Nuget..."
-dotnet nuget push "${files[0]}" --api-key "$NugetApiKey" --source "$NugetSource" --no-service-endpoint
+dotnet nuget push "${files[0]}" -k "$NugetApiKey" -s "$NugetSource" --no-service-endpoint
 echo "Pushing packed package to GitHub Packages..."
-dotnet nuget push "${files[0]}" --api-key "$PackagesApiKey" --source "$PackagesSource" --no-service-endpoint
+dotnet nuget add source "$PackagesSource" -n github -u zedseven -p "$PackagesApiKey" --store-password-in-clear-text
+#dotnet nuget push "${files[0]}" -k "$PackagesApiKey" -s "$PackagesSource" --no-service-endpoint
+dotnet nuget push "${files[0]}" -s "github" --no-service-endpoint
 
 echo "Done!"
